853e9ff88d93baf2dccb9796e6e3a328bfd897cf
Tate Redding <tateredding@gmail.com>
2022-12-15 08:51:44 -0700

    object orientation

--- index.js
+++ index.js
@@ -1,80 +1,65 @@
-let totalGold = 0;
-let goldPerSecond = 0;
+// STATE
 
-const totalGoldDisplay = document.getElementById("total_gold");
-const goldPerSecondDisplay = document.getElementById("gold_per_second");
+const state = {
+    totalGold: 0,
+    goldPerSecond: 0,
+    upgrades: [
+        {
+            name: "FootSoldier",
+            cost: 10,
+            gps: 1,
+            total: 0
+        },
+        {
+            name: "Knight",
+            cost: 50,
+            gps: 5,
+            total: 0
+        },
+        {
+            name: "Mounted Knight",
+            cost: 150,
+            gps: 10,
+            total: 0
+        },
+        {
+            name: "Cannon",
+            cost: 500,
+            gps: 25,
+            total: 0
+        }
+    ],
+    messages: [
+        "Poke goblin, ???, profit!",
+        "You're pretty good at this!",
+        "Have you ever thought about doing this professionally?",
+        "Hey, maybe you should leave them alone for a while...",
+        "You're not actually killing them, are you?",
+        "I heard the developer of this game is very handsome.",
+        "This is your entire existance.",
+        "Those poor goblins, give it a rest!",
+        "Do you know what 'genocide' means?",
+        "Don't forget to sharpen your sword!"
+    ]
+}
 
-const upgradeTotalsDisplay = document.getElementsByClassName("upgrade_total");
-const upgradeTotals = Array.from(upgradeTotalsDisplay).map(element => Number(element.innerText.split(' ')[1]));
-
-const upgradeButtons = document.getElementsByClassName("upgrade_button");
-const costs = Array.from(upgradeButtons).map(element => Number(element.innerText.split(' ')[1].slice(0, element.innerText.split(' ')[1].length-1)));
-
-const gps = document.getElementsByClassName("gps");
-const gpsAmounts = Array.from(gps).map(element => Number(element.innerText.split(' ')[0]));
+// DOM SELECTORS
 
 const upgradeDiv = document.getElementById("upgrades");
 
+const totalGoldDisplay = document.getElementById("total_gold");
+const goldPerSecondDisplay = document.getElementById("gold_per_second");
+const upgradeTotalsDisplay = document.getElementsByClassName("upgrade_total");
+
+const upgradeButtons = document.getElementsByClassName("upgrade_button");
 const goblinButton = document.getElementById("goblin_button");
 
 const message = document.getElementById("changing_message");
-const messages = [
-    "Poke goblin, ???, profit!",
-    "You're pretty good at this!",
-    "Have you ever thought about doing this professionally?",
-    "Hey, maybe you should leave them alone for a while...",
-    "You're not actually killing them, are you?",
-    "I heard the developer of this game is very handsome.",
-    "This is your entire existance.",
-    "Those poor goblins, give it a rest!",
-    "Do you know what 'genocide' means?",
-    "Don't forget to sharpen your sword!"
-]
 
-const updateTotalGoldDisplay = () => {
-    totalGoldDisplay.innerText = `Total Gold: ${totalGold}`;
-    checkAffordability();
-}
-
-const incrementGold = () => {
-    totalGold += goldPerSecond;
-    updateTotalGoldDisplay();
-}
-
-const checkAffordability = () => {
-    for (let i = 0; i < upgradeButtons.length; i++) {
-        currList = upgradeButtons[i].classList;
-        (totalGold >= costs[i]) ? currList.remove("cant_afford") : currList.add("cant_afford");
-    }
-}
-
-const addUpgrade = (button) => {
-    const index = Array.from(upgradeButtons).indexOf(button);
-    totalGold -= Math.floor(costs[index]);
-    updateTotalGoldDisplay();
-
-    goldPerSecond += gpsAmounts[index];
-    goldPerSecondDisplay.innerText = `Gold per second: ${goldPerSecond}`;
-
-    upgradeTotals[index]++;
-    upgradeTotalsDisplay[index].innerText = `Total: ${upgradeTotals[index]}`;
-
-    costs[index] *= 1.2;
-    upgradeButtons[index].innerText = `Buy: ${Math.floor(costs[index])}g`;
-
-}
-
-checkAffordability();
-setInterval(incrementGold, 1000);
-setInterval(() => {
-    if (totalGold > 0) {
-        const messageIndex = Math.floor(Math.random() * messages.length);
-        message.innerText = messages[messageIndex];
-    }
-}, 10000);
+// EVENT LISTENERS
 
 goblinButton.addEventListener("click", () => {
-    totalGold++;
+    state.totalGold++;
     updateTotalGoldDisplay();
 });
 
@@ -83,3 +68,48 @@ upgradeDiv.addEventListener("click", (clickEvent) => {
         addUpgrade(clickEvent.target);
     }
 })
+
+// HELPER FUNCTIONS
+
+const updateTotalGoldDisplay = () => {
+    totalGoldDisplay.innerText = `Total Gold: ${state.totalGold}`;
+    checkAffordability();
+}
+
+const incrementGold = () => {
+    state.totalGold += state.goldPerSecond;
+    updateTotalGoldDisplay();
+}
+
+const checkAffordability = () => {
+    for (let i = 0; i < upgradeButtons.length; i++) {
+        currList = upgradeButtons[i].classList;
+        (state.totalGold >= state.upgrades[i].cost) ? currList.remove("cant_afford") : currList.add("cant_afford");
+    }
+}
+
+const addUpgrade = (button) => {
+    const index = Array.from(upgradeButtons).indexOf(button);
+    const upgrade = state.upgrades[index];
+    state.totalGold -= Math.floor(upgrade.cost);
+    updateTotalGoldDisplay();
+
+    state.goldPerSecond += upgrade.gps;
+    goldPerSecondDisplay.innerText = `Gold per second: ${state.goldPerSecond}`;
+
+    upgrade.total++;
+    upgradeTotalsDisplay[index].innerText = `Total: ${upgrade.total}`;
+
+    upgrade.cost *= 1.2;
+    upgradeButtons[index].innerText = `Buy: ${Math.floor(upgrade.cost)}g`;
+
+}
+
+checkAffordability();
+setInterval(incrementGold, 1000);
+setInterval(() => {
+    if (state.totalGold > 0) {
+        const messageIndex = Math.floor(Math.random() * state.messages.length);
+        message.innerText = state.messages[messageIndex];
+    }
+}, 10000);
